13f0945cfeece212be974f837122a554d3691380
Ben Kuhn <ben.s.kuhn@gmail.com>
2014-10-18 13:01:57 -0700

    Homebrew modifications

--- homebrew.zsh
+++ homebrew.zsh
@@ -1,6 +1,11 @@
 #!/bin/zsh
 
-# allow Homebrew bins to shadow system bins
-export PATH=/usr/local/bin:/usr/local/sbin:$PATH
+# allow Homebrew bins to shadow system bins and manpages
+export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
+# allow Homebrew bins to shadow system bins and manpages
+export MANPATH="/usr/local/bin:/usr/local/sbin:$MANPATH"
 # TODO make this line portable
 source /usr/local/bin/virtualenvwrapper.sh
+# Use GNU coreutils because they're better than OS X's
+export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
