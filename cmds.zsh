c4073760089026804a2dc67ab0a6d34ab2a320a2
Ben Kuhn <ben.s.kuhn@gmail.com>
2015-04-05 13:29:57 -0700

    ssha commands and better notifications

--- cmds.zsh
+++ cmds.zsh
@@ -8,3 +8,11 @@ gitpushbranch () {
     $BRANCH="`git rev-parse --abbrev-ref HEAD`"
     git checkout master && git fetch && git pull && git merge $BRANCH --ff-only && git push origin master
 }
+
+function ssha {
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $@
+}
+
+function scpa {
+    scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $@
+}
